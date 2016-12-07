a8d6332fc6cbdc667619fcfc2d8634b4381305eb
Yankee <649661542@qq.com>
2016-12-07 16:28:46 +0800

    dqn Ver0.1

--- MazeGame.py
+++ MazeGame.py
@@ -10,13 +10,14 @@ Objdll = CDLL("./libtool.so")
 
 num_to_people = 0.5
 num_to_wall = -1
-num_to_road = 0.1
+num_to_road = 0
 num_to_finish = 1
 
 num_board_postive = 6
 num_board_negetive = -5
 
-class mazeState():
+
+class Maze():
 
     maze = np.zeros([MAP_SIZE, MAP_SIZE])
 
@@ -26,11 +27,12 @@ class mazeState():
     # 创建一个新的迷宫 random_map为二维矩阵
     def createNewMaze(self):
         bufferMap = Objdll.createIntBuffer(MAP_SIZE * MAP_SIZE)
-        Objdll.getRandomMaze(bufferMap, 10, 10);
+        Objdll.getRandomMaze(bufferMap, 10, 10)
 
         for i in range(MAP_SIZE):
             for k in range(MAP_SIZE):
-                self.maze[i][k] = Objdll.getValue(bufferMap, (k + i * MAP_SIZE))
+                self.maze[i][k] = Objdll.getValue(
+                    bufferMap, (k + i * MAP_SIZE))
 
         Objdll.destroyIntBuffer(bufferMap)
         return self.maze
@@ -42,30 +44,31 @@ class mazeState():
         end_point_x = 0
         end_point_y = 0
         while (end_point_x >= MAP_SIZE - num_board_postive or end_point_y >= MAP_SIZE - num_board_postive or end_point_x <= num_board_postive or end_point_y <= num_board_postive
-         or end_point == -1 or self.maze[end_point_x][end_point_y] == -1):
+               or end_point == -1 or self.maze[end_point_x][end_point_y] == -1):
             end_point = int(np.random.random() * (MAP_SIZE ** 2))
             end_point_x = end_point / MAP_SIZE
             end_point_y = end_point % MAP_SIZE
 
-
-        start_point_x= 0
+        start_point_x = 0
         start_point_y = 0
         while(start_point_x >= MAP_SIZE - num_board_postive or start_point_y >= MAP_SIZE - num_board_postive or start_point_x <= num_board_postive or start_point_y <= num_board_postive
-         or self.maze[start_point_x][start_point_y] == -1):
-            start_point_x = end_point_x + np.random.randint(low=-maxDistance, high=maxDistance)
-            start_point_y = end_point_y + np.random.randint(low=-maxDistance, high=maxDistance)
-
+              or self.maze[start_point_x][start_point_y] == -1):
+            start_point_x = end_point_x + \
+                np.random.randint(low=-maxDistance, high=maxDistance)
+            start_point_y = end_point_y + \
+                np.random.randint(low=-maxDistance, high=maxDistance)
 
         return start_point_x, start_point_y, end_point_x, end_point_y
 
-    #x y分别代表““人”所在位置的横竖坐标，在地图上做标记后返回
+    # x y分别代表““人”所在位置的横竖坐标，在地图上做标记后返回
     def getCurrentImage(self, s_x, s_y, e_x, e_y):
 
         currtent_image = np.zeros([MAP_SIZE, MAP_SIZE])
 
         for i in range(MAP_SIZE):
             for j in range(MAP_SIZE):
-                currtent_image[i][j] = num_to_wall if self.maze[i][j] == -1 else num_to_road
+                currtent_image[i][j] = num_to_wall if self.maze[
+                    i][j] == -1 else num_to_road
 
         for i in range(num_board_negetive, num_board_postive):
             for j in range(num_board_negetive, num_board_postive):
@@ -76,7 +79,7 @@ class mazeState():
 
         return currtent_image
 
-    #计算reward矩阵,输入为终点坐标
+    # 计算reward矩阵,输入为终点坐标
     def calRewardMatrix(self, x, y):
         reward = np.zeros([MAP_SIZE, MAP_SIZE])
         max_distance = 0
@@ -91,14 +94,15 @@ class mazeState():
                 if (self.maze[i][j] == -1):
                     reward[i][j] = -100.0
                 elif self.maze[i][j] == 1:
-                    reward[i][j] = -(np.sqrt((i - x) ** 2 + (j - y) ** 2) / max_distance)
+                    reward[i][j] = - \
+                        (np.sqrt((i - x) ** 2 + (j - y) ** 2) / max_distance)
                 #reward[i][j] = (reward[i][j] / 100.0)
         reward[x][y] = 100.0
 
         return reward
 
-    #返回一个一个操作对应的reward值和是否结束
-    def getReward(self, reward,  x, y):
+    # 返回一个一个操作对应的reward值和是否结束
+    def getReward(self, reward, x, y):
         terminated = False
         if reward[x][y] == -100.0 or reward[x][y] == 100.0:
             terminated = True
@@ -109,15 +113,28 @@ class mazeState():
 
         for i in range(MAP_SIZE):
             for j in range(MAP_SIZE):
-                Objdll.setValue(bufferMapR, j + i * MAP_SIZE, Objdll.rgb(0, int(np.abs(m[0][i][j] * 255)), 0))
+                Objdll.setValue(bufferMapR, j + i * MAP_SIZE,
+                                Objdll.rgb(0, int(np.abs(m[0][i][j] * 255)), 0))
 
         Objdll.initRenderer(800, 800, 32, 8)
         Objdll.render(bufferMapR, 100, 100)
 
         Objdll.handleEvents()
-        time.sleep(0.1)  # 0.1s
+        # time.sleep(0.1)  # 0.1s
 
         Objdll.destroyIntBuffer(bufferMapR)
+
+    def moveToNextState(self, action):
+        terminated = False
+        val = Objdll.move(action)
+        if val == -1:
+            terminated = True
+
+        return terminated, val
+
+    def getMazeState():
+        image = Objdll.getImage()
+        return image
 
 
 def __main__():
@@ -134,7 +151,3 @@ def __main__():
 
 if __name__ == "__main__":
     __main__()
-
-
-
-
---
+++ dqn.py
@@ -0,0 +1,178 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+import time
+import MazeGame as mg
+from keras.models import Sequential
+from keras.initializations import normal
+from keras.layers.core import Dense, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import Adam
+from collections import deque
+from random import sample
+import os
+
+
+imgRow, imgCol = 100, 100
+imgChannel = 4
+actionNum = 4
+initDistance = 1
+batchSz = 64
+gamma = 0.99
+observe = 3200
+replayMemory = 20000
+Epsilo = 0.3
+resetLimitaion = 200000
+
+
+def getModel():
+    model = Sequential()
+    model.add(Convolution2D(128, 5, 5, subsample=(2, 2), init=lambda shape, name: normal(
+        shape, scale=0.01, name=name), border_mode='same', input_shape=(imgChannel, imgRow, imgCol)))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(64, 3, 3, subsample=(2, 2), init=lambda shape,
+                            name: normal(shape, scale=0.01, name=name), border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(Convolution2D(32, 2, 2, subsample=(2, 2), init=lambda shape,
+                            name: normal(shape, scale=0.01, name=name), border_mode='same'))
+    model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(512, init=lambda shape,
+                    name: normal(shape, scale=0.01, name=name)))
+    model.add(Activation('relu'))
+    model.add(Dense(actionNum, init=lambda shape,
+                    name: normal(shape, scale=0.01, name=name)))
+
+    adam = Adam(lr=1e-5)
+    model.compile(loss='mse', optimizer=adam)
+    return model
+
+
+class imgQueue:
+    def __init__(self):
+        self.__queue = deque()
+        self.__info = deque()
+
+    def append(self, img):
+        if len(self.__queue) >= replayMemory:
+            self.__queue.popleft()
+        self.__queue.append(img)
+
+    def addInfo(self, info):
+        if len(self.__info) >= replayMemory - imgChannel:
+            self.__info.popleft()
+        self.__info.append(info)
+
+    def getChannels(self, position=-1):
+        grayImages_t = np.empty((1, imgChannel, imgRow, imgCol))
+
+        position = len(self.__queue) - \
+            imgChannel if position == -1 else position
+
+        for i in range(0, imgChannel):
+            # get last 4 image
+            grayImages_t[0][i] = self.__queue[i + position]
+
+        return grayImages_t
+
+    def getInfo(self, position=-1):
+        return self.__info[position]
+
+    def __len__(self):
+        return len(self.__queue) - imgChannel + 1
+
+    def resetQueue(self):
+        self.__queue.clear()
+        self.__info.clear()
+
+
+def train(model):
+
+    if os.path.exists("./model.data")
+        model.load_weights("model.data")
+
+    # Create a new maze
+    mz = mg.Maze()
+    mz.createNewMaze()
+
+    # pick random start & end postion
+    init_image = mz.getMazeState()
+
+    # initialized two counters
+    counter = -1
+    reset_time = -1
+    # set epsilp-random rate
+    randomEpsilon = Epsilo
+    # initialized a imag manager
+    queueImg = imgQueue()
+
+    inputs = np.zeros((batchSz, imgChannel, imgRow, imgCol))
+    targets = np.zeros((batchSz, actionNum))
+
+    while(True):
+        # initalized some flags
+        reset_time += 1
+        counter += 1
+        queueImg.resetQueue()
+        # Create continual 4 gray image
+        for i in range(imgChannel):
+            queueImg.append(init_image)
+
+        while(counter < resetLimitaion):
+
+            # making decision
+            if np.random.random() < randomEpsilon:
+                action_t = np.random.randint(0, actionNum)
+            else:
+                grayImages_t = queueImg.getChannels()
+                predict_action = model.predict(grayImages_t)
+                action_t = np.argmax(predict_action)  # reward预测值最大的那一步
+
+            # change the postion depends on action index
+            terminated, reward_t = mz.moveToNextState(action_t)
+            # get the next state
+            image = mz.getMazeState()
+            mz.visualization(image)
+            # add the newest state
+            queueImg.append(image)
+
+            if terminated:
+                # if knock into the wall
+                reward_t = -100.0
+            else:
+                # calculate reward
+                grayImages_t = queueImg.getChannels()
+                reward_t = -1.0 + gamma * np.max(model.predict(grayImages_t))
+
+            queueImg.addInfo((action_t, reward_t))
+
+            if counter < observe:
+                continue
+            #============TRAIN============
+
+            for i in range(batchSz):
+                choise = np.random.randint(0, len(queueImg) - 1)
+                inputs[i] = queueImg.getChannels(choise)
+                info = queueImg.getInfo(choise)
+                action_history = info[0]
+                reward_history = info[1]
+                targets[i] = model.predict(
+                    inputs[i].reshape((1, imgChannel, imgRow, imgCol)))
+                targets[i][action_history] = reward_history
+
+            loss = model.train_on_batch(inputs, targets)
+
+            if counter % 10 == 0:
+                print "loss = %.4f" % loss
+
+            if counter % 1000 == 0:
+                # save the weight
+                model.save_weights("model.data", overwrite=True)
+
+
+def __main__():
+    model = getModel()
+    train(model)
+
+
+if __name__ == "__main__":
+    __main__()
